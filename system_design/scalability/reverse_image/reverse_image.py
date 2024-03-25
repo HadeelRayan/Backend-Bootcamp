c65701a34f7a00496252ecbf1ee562e6e6c2e839
HadeelRayan <hadeel.rayan@campus.technion.ac.il>
2024-03-25 11:54:39 +0200

    initial commit

---
+++ system_design/scalability/reverse_image/reverse_image.py
@@ -0,0 +1,24 @@
+from PIL import Image
+import numpy as np
+import time
+
+
+def main():
+
+    image_path = 'img1.jpg'
+    original_image = Image.open(image_path)
+    start_time = time.time()
+
+    # Perform the color inversion
+    inverted_image = Image.eval(original_image, lambda x: 255 - x)
+
+    end_time = time.time()
+    execution_time_single_thread = end_time - start_time
+
+    inverted_image.save('inverted_img1.jpg')
+
+    print(execution_time_single_thread)
+
+
+if __name__ == "__main__":
+    main()
